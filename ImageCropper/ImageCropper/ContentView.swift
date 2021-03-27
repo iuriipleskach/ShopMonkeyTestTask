09c1eeb005eb52c6f0f2397c5b9cc04a423cf73a
Yuriy Pleskach <pleskachy@gmail.com>
2021-03-27 10:43:59 +0200

    Added default SwiftUI App Xcode project

---
+++ ImageCropper/ImageCropper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageCropper
+//
+//  Created by Yuriy Pleskach on 27.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageCropper/ImageCropper/ImageCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropperApp.swift
+//  ImageCropper
+//
+//  Created by Yuriy Pleskach on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
